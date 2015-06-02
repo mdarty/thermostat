e25d17f38cb4942c3f1aaccabed264fe31407fc4
Matthew Daugherty <mattd@daughertyengr.com>
2015-06-02 13:19:51 -0500

    Cleaned up thermo.py

--- thermo.py
+++ thermo.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python2
 import RPi.GPIO as GPIO
+import cPickle as pickle
 from time import sleep
-import datetime, sys, os, signal, threading, os.path, pywapi
-import Adafruit_DHT
-import ConfigParser
-import picamera, numpy
+from datetime import datetime, timedelta
+import sys, os, signal, threading, os.path, pywapi, redis, picamera, numpy, Adafruit_DHT, ConfigParser
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import redis
 
 Config = ConfigParser.ConfigParser()
 Config.read('/root/thermostat/config.ini')
@@ -80,7 +78,7 @@ class relay:
 
     def garage(self):
         GPIO.output(self.Garage_Pin, GPIO.LOW)
-        sleep(1)
+        sleep(0.5)
         GPIO.output(self.Garage_Pin, GPIO.HIGH)
 
     def cool(self):
@@ -138,11 +136,13 @@ class temp(threading.Thread):
         self.thermo = thermo()
         self.loop = True
         self.log_int = 15
-        self.log_time = datetime.datetime.now() - datetime.timedelta(minutes = self.log_int)
+        self.log_time = datetime.now() - timedelta(minutes = self.log_int)
         self.run_int = 15
-        self.run_time = datetime.datetime.now() - datetime.timedelta(minutes = self.run_int)
-        self.pic_int = 5
-        self.pic_time = datetime.datetime.now() - datetime.timedelta(minutes = self.pic_int)
+        self.run_time = datetime.now() - timedelta(minutes = self.run_int)
+        self.pic_int = 3
+        self.pic_time = datetime.now() - timedelta(minutes = self.pic_int)
+        self.sensor_int = 5
+        self.sensor_time = datetime.now() - timedelta(minutes = self.sensor_int)
         self.hostname = ["192.168.1.27", "192.168.1.3"]
         if Debug:
             print "Connecting to Redis"
@@ -151,12 +151,14 @@ class temp(threading.Thread):
         folder = "/tmp/thermo"
         if not os.path.exists(folder):
             os.makedirs(folder)
-        self.pipe.set('set_away_temp', self.thermo.set_away_temp)
-        self.pipe.set('set_temp', self.thermo.set_temp)
-        self.pipe.execute()
+        #self.pipe.set('set_away_temp', self.thermo.set_away_temp)
+        #self.pipe.set('set_temp', self.thermo.set_temp)
+        #self.pipe.execute()
         self.dev_type = Adafruit_DHT.DHT22
         self.dhtpin = int(4)
         self.log_list=[]
+
+        self.snooze=0.3
 
     def run(self):
         global Debug
@@ -169,51 +171,24 @@ class temp(threading.Thread):
             self.pipe.get('set_away_temp')
             self.pipe.get('set_away')
             self.pipe.get('garage')
+            self.thermo.mode, self.thermo.set_temp, self.thermo.state, self.thermo.set_away_temp, self.thermo.set_away, self.garage = self.pipe.execute()
 
-            self.thermo.mode, self.thermo.set_temp, self.thermo.state, self.thermo.set_away_temp, self.thermo.set_away, garage = self.pipe.execute()
+            sleep(self.snooze)
 
-            if Debug:
-                print "Garage: "+garage
-            if garage == "on":
-                if Debug:
-                    print "Opening Garage"
-                self.relay.garage()
-                garage = "off"
-                self.red.set('garage', garage)
-            else:
-                sleep(.5)
+            self.garage_action()
+
             if Debug:
                 print "Relay"
             self.thermo.run = self.relay.run
-            if Debug:
-                print "CPU Temp"
-            self.read_cpu_temp()
-            self.thermo.cpu_temp = self.cpu_temp
-            if Debug:
-                print "Sensor Read"
-            self.thermo.RH, self.thermo.T = Adafruit_DHT.read_retry(self.dev_type, self.dhtpin)
-            self.thermo.RH=round(self.thermo.RH, 2)
-            self.thermo.T=round(self.thermo.T*9/5+32, 2)
-            #self.thermo.THI = round(t-0.55*(1-h/100)*(t-58),1)
-            self.thermo.THI = self.thermo.T
-            if datetime.datetime.now() - self.run_time >= datetime.timedelta(minutes = self.run_int):
-                self.thermo.Tout, self.thermo.RHout = outdoor()
-            else:
-                sleep(0.5)
 
-            if datetime.datetime.now() - self.pic_time >= datetime.timedelta(minutes = self.pic_int):
-                with picamera.PiCamera() as camera:
-                    if Debug:
-                        print "Taking Picture"
-                    camera.led = False
-                    camera.start_preview()
-                    camera.vflip = True
-                    camera.hflip = True
-                    sleep(2)
-                    camera.capture('/tmp/thermo/image.jpg')
-                    self.pic_time = datetime.datetime.now()
+            if (datetime.now() - self.sensor_time >= timedelta(minutes = self.sensor_int)):
+                self.read_cpu_temp()
+                self.sensor()
+            elif (datetime.now() - self.pic_time >= timedelta(minutes = self.pic_int)):
+                self.take_pic()
             else:
-                sleep(0.5)
+                sleep(self.snooze)
+
             if Debug:
                 print "Updating Redis"
             self.pipe.set('cpu_temp', self.thermo.cpu_temp)
@@ -225,76 +200,124 @@ class temp(threading.Thread):
             self.pipe.set('run', self.thermo.run)
             self.pipe.execute()
 
+            sleep(self.snooze)
+
             if Debug:
                 print "Home/Away"
             if self.thermo.active == "auto":
                 self.home()
             else:
-                sleep(0.5)
+                sleep(self.snooze)
 
-            if self.thermo.state == "here" or self.thermo.state == "home":
-                self.hist = self.thermo.active_hist
-                self.desired_temp = self.thermo.set_temp
-            elif self.thermo.state == "away":
-                self.hist = self.thermo.inactive_hist
-                self.desired_temp = self.thermo.set_away_temp
-            else:
-                print "State broke"
-                self.thermo.state == "away"
-                self.hist = self.thermo.inactive_hist
-                self.desired_temp = self.thermo.set_away_temp
+            self.away_home()
 
-            if Debug:
-                print "Cool/Heat/Fan"
-            if self.thermo.mode == "cool":
-                if self.thermo.THI > self.desired_temp + self.hist and self.relay.run != "cool":
-                    self.relay.cool()
-                    self.run_time = datetime.datetime.now()
-                    self.log()
-                elif self.thermo.THI < self.desired_temp and self.relay.run == "cool" and datetime.datetime.now() - self.run_time >= datetime.timedelta(minutes = self.run_int):
-                    self.relay.fan()#spin down
-                    self.wait(30)
-                    self.relay.off()
-                    self.log()
-            elif self.thermo.mode == "heat":
-                if self.thermo.THI < self.desired_temp - self.hist and self.relay.run != "heat":
-                    self.relay.heat()
-                    self.run_time = datetime.datetime.now()
-                    self.log()
-                elif self.thermo.THI > self.desired_temp and self.relay.run == "heat" and datetime.datetime.now() - self.run_time >= datetime.timedelta(minutes = self.run_int):
-                    self.relay.fan()#spin down
-                    self.wait(30)
-                    self.relay.off()
-                    self.log()
-            elif self.thermo.mode == "off" and self.relay.run != "off":
-                self.relay.off()
-                self.log()
-            elif self.thermo.mode == "fan" and self.relay.run != "fan":
-                self.relay.fan()
-                self.log()
-            else:
-                sleep(0.1)
+            self.HVAC()
+
             if Debug:
                 print "Log Int"
-            if (datetime.datetime.now() - self.log_time >= datetime.timedelta(minutes = self.log_int)):
+            if (datetime.now() - self.log_time >= timedelta(minutes = self.log_int)):
                 self.log()
             else:
-                sleep(0.5)
+                sleep(self.snooze)
         del self.relay
 
-    def stop(self):
-        self.loop = False
+    def garage_action(self):
+        if Debug:
+            print "Garage: "+self.garage
+        if self.garage == "on":
+            if Debug:
+                print "Opening Garage"
+            self.relay.garage()
+            self.garage = "off"
+            self.red.set('garage', self.garage)
+        else:
+            sleep(self.snooze)
 
-    def __del__(self):
-        self.loop = False
+    def sensor(self):
+        if Debug:
+            print "Read outdoor"
+        self.thermo.Tout, self.thermo.RHout = outdoor()
+        if Debug:
+            print "Sensor Read"
+        RH, T = Adafruit_DHT.read_retry(self.dev_type, self.dhtpin)
+        try:
+            self.thermo.RH=round(RH, 2)
+            self.thermo.T=round(T*9/5+32, 2)
+            #self.thermo.THI = round(t-0.55*(1-h/100)*(t-58),1)
+            self.thermo.THI = self.thermo.T
+            self.sensor_time=datetime.now()
+        except:
+            print "Reading sensor failed"
+
+    def take_pic(self):
+        with picamera.PiCamera() as camera:
+            if Debug:
+                print "Taking Picture"
+            camera.led = False
+            camera.start_preview()
+            camera.vflip = True
+            camera.hflip = True
+            sleep(2)
+            camera.capture('/tmp/thermo/image.jpg')
+            self.pic_time = datetime.now()
+
+    def away_home(self):
+        if self.thermo.state == "here" or self.thermo.state == "home":
+            self.hist = self.thermo.active_hist
+            self.desired_temp = self.thermo.set_temp
+        elif self.thermo.state == "away":
+            self.hist = self.thermo.inactive_hist
+            self.desired_temp = self.thermo.set_away_temp
+        else:
+            print "State broke"
+            self.thermo.state == "away"
+            self.hist = self.thermo.inactive_hist
+            self.desired_temp = self.thermo.set_away_temp
+
+    def HVAC(self):
+        if Debug:
+            print "Cool/Heat/Fan"
+        if self.thermo.mode == "cool":
+            if self.thermo.THI > self.desired_temp + self.hist and self.relay.run != "cool":
+                self.relay.cool()
+                self.run_time = datetime.now()
+                self.log()
+            elif self.thermo.THI < self.desired_temp and self.relay.run == "cool" and datetime.now() - self.run_time >= timedelta(minutes = self.run_int):
+                self.relay.fan()#spin down
+                self.wait(30)
+                self.relay.off()
+                self.log()
+        elif self.thermo.mode == "heat":
+            if self.thermo.THI < self.desired_temp - self.hist and self.relay.run != "heat":
+                self.relay.heat()
+                self.run_time = datetime.now()
+                self.log()
+            elif self.thermo.THI > self.desired_temp and self.relay.run == "heat" and datetime.now() - self.run_time >= timedelta(minutes = self.run_int):
+                self.relay.fan()#spin down
+                self.wait(30)
+                self.relay.off()
+                self.log()
+        elif self.thermo.mode == "off" and self.relay.run != "off":
+            self.relay.off()
+            self.log()
+        elif self.thermo.mode == "fan" and self.relay.run != "fan":
+            self.relay.fan()
+            self.log()
+        else:
+            sleep(self.snooze)
 
     def log(self):
         global Debug
         if Debug:
             print "Log"
+
         print "desired_temp" + str(self.desired_temp)
         self.desired_temp = int(0)
-        self.log_list.append([datetime.datetime.now(), self.thermo.T, self.thermo.RH, self.thermo.Tout, self.thermo.RHout, self.thermo.THI, self.hist, self.desired_temp, self.thermo.state, self.thermo.mode, self.relay.run])
+        if len(self.log_list) == 0 and os.path.isfile("/tmp/thermo/log.pickle"):
+            self.log_list=pickle.load(open("/tmp/thermo/log.pickle", "rb"))
+
+        self.log_list.append([datetime.now(), self.thermo.T, self.thermo.RH, self.thermo.Tout, self.thermo.RHout, self.thermo.THI, self.hist, self.desired_temp, self.thermo.state, self.thermo.mode, self.relay.run])
+
         with open("/tmp/thermo/thermo.log", "a") as myfile:
             string=''
             for i in self.log_list[-1]:
@@ -302,12 +325,13 @@ class temp(threading.Thread):
             string[:-1]
             string+='\n'
             myfile.write(string)
+
         if len(self.log_list)>60*24/15:
             print self.log_list.pop(0)
         else:
             print "{}/{}".format(len(self.log_list), 60*24/15)
 
-        self.log_time = datetime.datetime.now()
+        self.log_time = datetime.now()
 
         entrys = len(self.log_list)
 
@@ -336,10 +360,20 @@ class temp(threading.Thread):
     def home(self):
         self.state = "away"
         for h in self.hostname:
-                response = os.system("ping -c 1 " + h)
-                if response == 0:
-                        self.state = "here"
-                        break
+            response = os.system("ping -c 1 " + h)
+            if response == 0:
+                self.state = "here"
+                break
+
+    def read_cpu_temp(self):
+        if Debug:
+            print "CPU Temp"
+
+        with open('/sys/class/thermal/thermal_zone0/temp', 'r') as temp_file:
+            self.cpu_temp = int(temp_file.read()) * 9 / 5000 + 32
+        if self.cpu_temp > 185:
+            print "I'm burning up"
+        self.thermo.cpu_temp = self.cpu_temp
 
     def wait(self, time):
         for i in range(time):
@@ -348,12 +382,13 @@ class temp(threading.Thread):
                 del self.relay
                 break
 
-    def read_cpu_temp(self):
-        temp_file = open('/sys/class/thermal/thermal_zone0/temp', 'r')
-        self.cpu_temp = int(temp_file.read()) * 9 / 5000 + 32
-        temp_file.close()
-        if self.cpu_temp > 185:
-            print "I'm burning up"
+    def stop(self):
+        self.loop = False
+
+    def __del__(self):
+        pickle.dump(self.log_list, open("/tmp/thermo/log.pickle", "wb"))
+        self.loop = False
+
 
 def main():
     global Debug
